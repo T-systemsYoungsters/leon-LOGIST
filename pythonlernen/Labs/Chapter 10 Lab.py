9a30dece3a026e3866a82eda03a1ef8571eab079
Lmsr92 <leon.moser@telekom.de>
2023-02-25 16:56:07 +0100

    Chapter 10 Lab

--- pythonlernen/Labs/Chapter 10 Lab.py
+++ pythonlernen/Labs/Chapter 10 Lab.py
@@ -9,6 +9,7 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 STRASSE = (64,64,64)
 LIGHT_BLUE =(0,191,255)
+GREY = (224,224,224)
 
 pi = 3.141592653
 
@@ -17,6 +18,9 @@ plane_y_coord = 300
 
 plane_x_speed = 0
 plane_y_speed = 0 
+
+
+
 
 
 def draw_streifen(i):
@@ -29,19 +33,44 @@ def draw_plane(x,y):
     pygame.draw.ellipse(screen, BLACK,[x,y, 80, 200],2)
     pygame.draw.arc(screen, WHITE, [x+30, y+15, 20,20], 2*pi,pi, 4)
 
-    #wings left
+    #wing left
     pygame.draw.polygon(screen, LIGHT_BLUE,[[x+3,y+60],[x-70, y+135],[x-65,y+140],[x+2,y+125]] )
     pygame.draw.polygon(screen, BLACK,[[x+3,y+60],[x-70, y+135],[x-65,y+140],[x+2,y+125]], 2)
 
-    # pygame.draw.polygon(screen, LIGHT_BLUE,[[x+35-13,y+220-40],[x+10-13, y+230-40],[x+15-13,y+232-40],[x+32-10,y+228-40]] )
-    # pygame.draw.polygon(screen, BLACK,[[x+35-13,y+220-40],[x+10-13, y+230-40],[x+15-13,y+232-40],[x+32-10,y+228-40]] ,2)
-    #wings right
+    #wing right
     pygame.draw.polygon(screen, LIGHT_BLUE,[[x+80-3,y+60],[x+80+70, y+135],[x+80+65,y+140],[x-2+80,y+125]] )
     pygame.draw.polygon(screen, BLACK,[[x+80-3,y+60],[x+80+70, y+135],[x+80+65,y+140],[x-2+80,y+125]],2 )
 
-    #pygame.draw.polygon(screen, LIGHT_BLUE,[[x-30,20],[x-15, 30],[x-20,32],[x-32,28]] )
+    #windows
+    for i in range(0, 80, 10):
+        pygame.draw.rect(screen, WHITE, [x+10, y+50 +i,5,5])
+        pygame.draw.rect(screen, WHITE, [x+65, y+50 +i,5,5])
 
-    #pygame.draw.polygon(screen, LIGHT_BLUE,[[600,400],[550, 450],[555,455],[600,425]], 2)
+def draw_helicopter(x,y):
+
+    #body
+    pygame.draw.circle(screen, GREY,[x+50,y+12.5],10)
+    pygame.draw.circle(screen, BLACK,[x+50,y+12.5],10,2)
+    pygame.draw.rect(screen, RED, [x,y,50,25])
+    pygame.draw.rect(screen, BLACK, [x,y,50,25],2)
+    pygame.draw.rect(screen, GREY, [x+30, y+5,15,10])
+    pygame.draw.rect(screen, GREY, [x+10, y+5,15,10])
+    pygame.draw.rect(screen, RED, [x-25,y+5,30,7])
+    pygame.draw.rect(screen, BLACK, [x-25,y+5,30,7], 2)
+    
+    #rotor
+    pygame.draw.line(screen,BLACK, [x+25,y],[x+25,y-7],3)
+    pygame.draw.line(screen, BLACK, [x-10, y-10], [x+60,y-10],4)
+
+    #backrotor
+    pygame.draw.circle(screen,RED, [x-25,y+8],8,)
+    pygame.draw.circle(screen,BLACK, [x-25,y+8],8,4)
+
+    pygame.draw.line(screen,BLACK, [x+5,y+25],[x+5, y+35],2)
+    pygame.draw.line(screen,BLACK, [x+45,y+25],[x+45, y+35],2)
+    pygame.draw.line(screen,BLACK,[x-5,y+37], [x+60, y+37],3)
+
+
 
 pygame.init()
  
@@ -53,7 +82,9 @@ pygame.display.set_caption("Planerun")
 
 done = False
 clock = pygame.time.Clock()
- 
+
+pygame.mouse.set_visible(0)
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -83,7 +114,12 @@ while not done:
 
     # --- Game logic should go here
     plane_x_coord += plane_x_speed
-    plane_y_coord += plane_y_speed   
+    plane_y_coord += plane_y_speed  
+
+    pos = pygame.mouse.get_pos()
+    helicopter_x_coord = pos[0]
+    helicopter_y_coord = pos[1]
+
     # If you want a background image, replace this clear with blit'ing the
     # background image.
     screen.fill(STRASSE)
@@ -92,6 +128,8 @@ while not done:
         draw_streifen(i)
     
     draw_plane(plane_x_coord, plane_y_coord)
+
+    draw_helicopter(helicopter_x_coord,helicopter_y_coord)
 
     pygame.display.flip()
     
